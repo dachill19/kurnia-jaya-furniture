7cabac107da3a406237f17ce5d1bce812567ad1c
dachill19 <daffa.hasanal@gmail.com>
2025-04-14 10:35:24 +0700

    start frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { LanguageProvider } from "@/context/LanguageContext";
+import { CartProvider } from "@/context/CartContext";
+
+import Layout from "./components/Layout";
+
+const App = () => (
+  <LanguageProvider>
+    <CartProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+    </CartProvider>
+  </LanguageProvider>
+);
+
+export default App
---
+++ frontend/src/components/Layout.tsx
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
+
+const Layout = () => {
+  const location = useLocation();
+
+  // Scroll to top when route changes
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
+  return (
+    <div className="flex flex-col min-h-screen bg-gray-50 transition-colors duration-300">
+      <Header />
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;

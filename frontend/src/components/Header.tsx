4d86ea25d3096d1f203791d46f5dcb5d3364de09
dachill19 <daffa.hasanal@gmail.com>
2025-04-14 18:12:59 +0700

    get categories from api

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { startCase } from "lodash";
 import axios from "axios";
 import { useLanguage } from "@/context/LanguageContext";
 import { useCart } from "@/context/CartContext";
@@ -176,7 +177,7 @@ const Header = () => {
                   {categories.map((category) => (
                     <DropdownMenuItem asChild key={category.id}>
                       <Link to={`/categories/${category.name.toLowerCase()}`} className="cursor-pointer">
-                        {category.name}
+                        {startCase(category.name)}
                       </Link>
                     </DropdownMenuItem>
                   ))}
